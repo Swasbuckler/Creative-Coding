1df90aaf499fdaa99bee4d393ea98da9f3a6002d
Swasbuckler <shahandyhadian@gmail.com>
2026-03-26 01:07:35 +0700

    Updated ThreeJS Suspense Element

--- src/lib/components/ThreeJSSuspenseElement.tsx
+++ src/lib/components/ThreeJSSuspenseElement.tsx
@@ -1,11 +1,13 @@
 import { Html } from "@react-three/drei";
+import { IconLoader2 } from "@tabler/icons-react";
 
 export default function ThreeJSSuspenseElement() {
 
   return (
     <Html center>
-      <div className="text-xs sm:text-sm">
-        Loading...
+      <div className="h-6 flex flex-row justify-center items-center gap-1 text-base">
+        <IconLoader2 className="size-6 animate-spin" />
+        <span>Loading...</span>
       </div>
     </Html>
   );
